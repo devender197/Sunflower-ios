a5bf85fc8df18e6fe36a4d8c99e1c6dc5bbcc797
devender <devender197@gmail.com>
2025-06-05 22:16:25 -0700

    Initial Commit

---
+++ Sunflower-ios/Sunflower_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sunflower_iosApp.swift
+//  Sunflower-ios
+//
+//  Created by Devender Sinhg Parihar on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Sunflower_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sunflower-iosTests/Sunflower_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sunflower_iosTests.swift
+//  Sunflower-iosTests
+//
+//  Created by Devender Sinhg Parihar on 6/5/25.
+//
+
+import Testing
+@testable import Sunflower_ios
+
+struct Sunflower_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
